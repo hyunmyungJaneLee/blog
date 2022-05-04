082e4134eb037c4f488f25986d37ab89e38bafd2
이현명 <hm.lee@autowini.com>
2022-05-05 00:38:27 +0900

    globalStyle을 적용한다. 프로필 사진을 적용한다. 기본 폴더/파일을 생성한다.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,22 @@
 import React, { FunctionComponent } from 'react';
 import Text from 'components/Text';
 import { Link } from 'gatsby';
+import GlobalStyle from 'components/Common/GlobalStyle';
+import ProfileImage from 'components/Main/Profile';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`;
 
 const IndexPage: FunctionComponent = function () {
   return (
-    <div>
-      <Text text="Hello world!" />
-      <Link to="/info">To Info</Link>
-    </div>
+    <Container>
+      <GlobalStyle />
+      <ProfileImage />
+    </Container>
   );
 };
 
