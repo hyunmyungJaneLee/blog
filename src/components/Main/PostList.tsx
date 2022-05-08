d62e856c364ba393b03db127583db2bb887c301c
이현명 <hm.lee@autowini.com>
2022-05-09 00:10:35 +0900

    게시물 스타일링 추가한다.

--- src/components/Main/PostList.tsx
+++ src/components/Main/PostList.tsx
@@ -15,6 +15,56 @@ const PostList: FunctionComponent = function () {
   return (
     <PostListWrapper>
       <PostItem
+        title="Post Item Title... Post Item Title...Post Item Title...Post Item Title...Post Item Title...Post Item Title..."
+        date="2020.01.29."
+        categories={[
+          'Web',
+          'Frontend',
+          'Testing',
+          'Web',
+          'Frontend',
+          'Testing',
+          'Web',
+          'Frontend',
+          'Testing',
+        ]}
+        summary="Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident repellat doloremque fugit quis rem temporibus! Maxime molestias, suntrem debitis odit harum impedit. Modi cupiditate harum dignissimos eos in corrupti!"
+        thumbnail="NOIMAGE"
+        link="link"
+      />
+      <PostItem
+        title="title"
+        date="data"
+        categories={['web', 'sdfsdf', 'sdfsfd']}
+        summary="summary"
+        thumbnail="NOIMAGE"
+        link="link"
+      />
+      <PostItem
+        title="title"
+        date="data"
+        categories={['web', 'sdfsdf', 'sdfsfd']}
+        summary="summary"
+        thumbnail="NOIMAGE"
+        link="link"
+      />
+      <PostItem
+        title="title"
+        date="data"
+        categories={['web', 'sdfsdf', 'sdfsfd']}
+        summary="summary"
+        thumbnail="NOIMAGE"
+        link="link"
+      />
+      <PostItem
+        title="title"
+        date="data"
+        categories={['web', 'sdfsdf', 'sdfsfd']}
+        summary="summary"
+        thumbnail="NOIMAGE"
+        link="link"
+      />
+      <PostItem
         title="title"
         date="data"
         categories={['web', 'sdfsdf', 'sdfsfd']}
