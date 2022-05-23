58c12197e2ad151981458de9a725dbfa4eac3c64
이현명 <hm.lee@autowini.com>
2022-05-23 21:35:23 +0900

    slug 생성을 위한 설정을 추가한다.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,12 +1,12 @@
 exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
+  const { createPage } = actions;
   createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
+    path: '/using-dsg',
+    component: require.resolve('./src/templates/using-dsg.js'),
     context: {},
     defer: true,
-  })
-}
+  });
+};
 
 /**
  * Implement Gatsby's Node APIs in this file.
@@ -16,20 +16,31 @@ exports.createPages = async ({ actions }) => {
 
 // You can delete this file if you're not using it
 
-const path = require("path")
+const path = require('path');
 
 // Setup Import Alias
 exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
-  const output = getConfig().output || {}
+  const output = getConfig().output || {};
 
   actions.setWebpackConfig({
     output,
     resolve: {
       alias: {
-        components: path.resolve(__dirname, "src/components"),
-        utils: path.resolve(__dirname, "src/utils"),
-        hooks: path.resolve(__dirname, "src/hooks"),
+        components: path.resolve(__dirname, 'src/components'),
+        utils: path.resolve(__dirname, 'src/utils'),
+        hooks: path.resolve(__dirname, 'src/hooks'),
       },
     },
-  })
-}
+  });
+
+  // Generate a Slug Each Post Data
+  exports.onCreateNode = ({ node, getNode, actions }) => {
+    const { createNodeField } = actions;
+
+    if (node.internal.type === `MarkdownRemark`) {
+      const slug = createFilePath({ node, getNode });
+
+      createNodeField({ node, name: 'slug', value: slug });
+    }
+  };
+};
