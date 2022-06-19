6bbff53d420401c6b2dd58aa237da44ebd2cdef8
이현명 <hm.lee@autowini.com>
2022-06-19 20:25:55 +0900

    sitemap 동적 생성 플러그인 적용, robots.txt 동적 생성 플러그인 적용

--- gatsby-config.js
+++ gatsby-config.js
@@ -83,6 +83,13 @@ module.exports = {
               stripQueryString: true,
             },
           },
+          'gatsby-plugin-sitemap',
+          {
+            resolve: 'gatsby-plugin-robots-txt',
+            options: {
+              policy: [{ userAgent: '*', allow: '/' }],
+            },
+          },
         ],
       },
     },
